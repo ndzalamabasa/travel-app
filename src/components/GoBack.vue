12be381b4dac4a1d004de8f291a06c40e7d3aee2
ndzalamabasa <ndzalamabasa@gmail.com>
2024-07-09 09:37:06 +0200

    add go back button

---
+++ src/components/GoBack.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <span class="go-back">
+    <button @click="$router.back()">go back</button>
+  </span>
+</template>
+
+<style scoped></style>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -2,6 +2,7 @@
 import { computed, ref } from 'vue'
 import sourceData from '@/data.json'
 import ExperienceCard from '@/components/ExperienceCard.vue'
+import GoBack from '@/components/GoBack.vue';
 const props = defineProps<{
   id: number
 }>()
@@ -13,28 +14,30 @@ const destination = computed(() => destinations.value.find((d) => d.id === props
 
 <template>
   <section class="destination">
-    <h1>{{ destination.name }}</h1>
-    <div class="destination-details">
-      <img :src="`/src/assets/images/${destination.image}`" :alt="destination.name" />
-      <p>{{ destination.description }}</p>
+    <h1>{{ destination?.name }}</h1>
+    <GoBack />
+    <div class="destination?-details">
+      <img :src="`/src/assets/images/${destination?.image}`" :alt="destination?.name" />
+      <p>{{ destination?.description }}</p>
     </div>
   </section>
   <section class="experiences">
-    <h2>Top Experiences in {{ destination.name }}</h2>
+    <h2>Top Experiences in {{ destination?.name }}</h2>
 
     <div class="cards">
       <router-link
-        v-for="experience in destination.experiences"
+        v-for="experience in destination?.experiences"
         :key="experience.slug"
         :to="{
           name: 'experience.show',
-          params: { id: destination.id, slug: destination.slug, experienceSlug: experience.slug }
+          params: { id: destination?.id, slug: destination?.slug, experienceSlug: experience.slug }
         }"
       >
         <ExperienceCard :experience="experience" />
       </router-link>
     </div>
   </section>
+  <router-view />
 </template>
 
 <style></style>
