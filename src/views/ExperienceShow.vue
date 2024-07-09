9b2b769803fc9f9d4aac8bc7bf885e43bb164652
ndzalamabasa <ndzalamabasa@gmail.com>
2024-07-09 09:36:48 +0200

    fix type errorS

--- src/views/ExperienceShow.vue
+++ src/views/ExperienceShow.vue
@@ -11,16 +11,16 @@ const destinations = ref(sourceData.destinations)
 const destination = computed(() => destinations.value.find((d) => d.id === props.id))
 
 const experience = computed(() =>
-  destination.value.experiences.find((e) => e.slug === props.experienceSlug)
+  destination.value?.experiences.find((e) => e.slug === props.experienceSlug)
 )
 </script>
 
 <template>
   <section>
-    <h1>{{ experience.name }}</h1>
+    <h1>{{ experience?.name }}</h1>
     <div>
-      <img :src="`/src/assets/images/${experience.image}`" :alt="experience.name" />
-      <p>{{ experience.description }}</p>
+      <img :src="`/src/assets/images/${experience?.image}`" :alt="experience?.name" />
+      <p>{{ experience?.description }}</p>
     </div>
   </section>
 </template>
